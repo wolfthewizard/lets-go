fc535f6d5fc0b58e5c888a7fe36d36f0ef03990b
Bialy6000 <danielwz99@gmail.com>
2019-12-13 11:00:33 +0100

    changed server handling

--- lets-go.server/src/main/java/core/ICommandDirector.java
+++ lets-go.server/src/main/java/core/ICommandDirector.java
@@ -7,5 +7,6 @@ public interface ICommandDirector {
         CreateNewBotGameResult CreateNewBotGame(boolean letBotStart, BoardSize boardSize);
         int CreateNewMultiplayerGame();
         MoveResponse TryToMove(Move move);
+        MoveResponse GetBotMove(int gameId);
         void CancelGame(MoveIdentity leftIdentity);
 }
