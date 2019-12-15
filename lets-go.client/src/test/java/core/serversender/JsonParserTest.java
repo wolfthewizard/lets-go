2f79bdb17035c008dce4d6098e752fce4e4c73d3
Bialy6000 <danielwz99@gmail.com>
2019-12-15 16:44:10 +0100

    jsonparser tests

---
+++ lets-go.client/src/test/java/core/serversender/JsonParserTest.java
@@ -0,0 +1,42 @@
+package core.serversender;
+
+import contract.ActionDTO;
+import contract.ResponseDTO;
+import contract.enums.BoardSize;
+import contract.enums.ResponseType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonParserTest {
+
+    private JsonParser jsonParser;
+
+    @BeforeEach
+    public void setUp(){
+        jsonParser = new JsonParser();
+    }
+
+    @Test
+    public void parseJsonToResponse_ParseCorrectly() {
+
+        String json = "{ \"responseType\": \"SERVER_ERROR\" }";
+
+        ResponseDTO result = jsonParser.parseJsonToResponse(json);
+
+        ResponseDTO expected = new ResponseDTO(ResponseType.SERVER_ERROR);
+        assertEquals(expected.getResponseType(), result.getResponseType());
+        assertEquals( expected.getChanges(), result.getChanges());
+
+    }
+
+    @Test
+    public void parseActionToJson_ParseCorrectly() {
+
+        ActionDTO actionDTO = new ActionDTO(BoardSize.NINE);
+
+        String result = jsonParser.parseActionToJson(actionDTO);
+
+        assertEquals(result, "{\"actionType\":\"STARTGAME\",\"boardSize\":\"NINE\"}");
+    }
+}
